18a7248faf58d7a883d9a56413960129472efe3f
Nipun Tiwari <happynipun2@gmail.com>
2025-06-12 16:59:59 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -12,4 +12,14 @@ app.use(express.json({limit: "20kb"}))
 app.use(express.urlencoded({extended: true, limit:"20kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//routes
+
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+
+
+//https://localhost:8000/api/v1/users/
 export { app }
